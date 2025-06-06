d68927d4aac1662f00c06b6665e0aa25fa0eb995
Adarsh Shrivastav <aadarshshrivastav01@gmail.com>
2025-06-06 11:56:01 +0530

    design

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = () => {
     <div className="h-20 bg-white border-b border-gray-200 backdrop-blur-md px-4 md:px-0 sticky top-0 z-30 shadow-sm">
       <div className="container mx-auto flex items-center justify-between gap-5 h-full">
         <Link to="/dashboard" className="py-2">
-          <h2 className="text-lg md:text-xl font-semibold text-gray-900 tracking-tight">
+          <h2 className="text-lg md:text-xl font-semibold text-gray-900 tracking-tight px-5">
             PrepMate AI
           </h2>
         </Link>
 
-        <div className="py-2">
+        <div className="py-2 px-5">
           <ProfileInfoCard />
         </div>
       </div>
